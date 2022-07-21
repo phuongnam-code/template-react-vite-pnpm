25ec3af297b1525f2c662937b6209e9f28e84691
PhuongNam <nguyenducnam.it@gmail.com>
2022-07-21 18:19:58 +0700

    fix name css

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,36 @@ import routes from '@/routes';
 import { uuid } from '@/utils/commonFun';
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <nav className="nav">
-          {routes.map((route) => {
-            const { path, name, hidden } = route;
-            return (
-              !hidden && (
-                <Link key={uuid()} to={path} style={{ padding: 5 }}>
-                  {name}
-                </Link>
-              )
-            );
-          })}
-        </nav>
-        <div className="wraper">
-          <ErrorBoundaryPage>
-            <Suspense fallback={<div>Loading...</div>}>
-              <Routes>
-                {routes.map((route) => {
-                  const { path, component: Component } = route;
-                  return <Route key={uuid()} path={path} element={<Component />} />;
-                })}
-              </Routes>
-            </Suspense>
-          </ErrorBoundaryPage>
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				<nav className='nav'>
+					{routes.map((route) => {
+						const { path, name, hidden } = route;
+						return (
+							!hidden && (
+								<Link key={uuid()} to={path} style={{ padding: 5 }}>
+									{name}
+								</Link>
+							)
+						);
+					})}
+				</nav>
+				<div className='wrapper'>
+					<ErrorBoundaryPage>
+						<Suspense fallback={<div>Loading...</div>}>
+							<Routes>
+								{routes.map((route) => {
+									const { path, component: Component } = route;
+									return <Route key={uuid()} path={path} element={<Component />} />;
+								})}
+							</Routes>
+						</Suspense>
+					</ErrorBoundaryPage>
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
